4e78c7e9871dc5a9086d07d508917ab70bdbf710
Daisuke Sakurai <d.sakurai0@gmail.com>
2026-03-22 01:56:55 +0000

    Maxima demo

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-# Create virtual environment in .venv
-uv venv --allow-existing .venv
-
-# Activate virtual environment and upgrade pip
-# source .venv/bin/activate
-# uv pip install --upgrade pip
-# 
-
 mkdir -p ~/.config/nix
 printf 'experimental-features = nix-command flakes\n' >> ~/.config/nix/nix.conf
 
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from maxima-jupyter!")
-
-
-if __name__ == "__main__":
-    main()
